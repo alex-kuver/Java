510fbb36722dd32969aac401918f7f009223b6e5
alex-kuver <63493443+alex-kuver@users.noreply.github.com>
2020-05-18 12:50:08 +0300

    Add files via upload
    Лабораторная 6

---
+++ lab06/src/main/java/com/vlsu/lab06/node/operations/MultNode.java
@@ -0,0 +1,26 @@
+package com.vlsu.lab06.node.operations;
+
+import com.vlsu.lab06.node.INode;
+
+public class MultNode implements INode {
+    private final INode leftNode;
+    private final INode rightNode;
+
+    public MultNode(INode leftNode, INode rightNode) {
+        this.leftNode = leftNode;
+        this.rightNode = rightNode;
+    }
+
+    public int calculate() {
+        int leftValue = leftNode.calculate();
+        if (leftValue == 0)
+            return 0;
+        else
+            return leftValue * rightNode.calculate();
+    }
+
+    public String toString() {
+        return "(" + leftNode.toString() + ") * (" + rightNode.toString() + ")";
+    }
+
+}
